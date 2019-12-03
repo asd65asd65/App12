0f10091f68085bb4a6d8faa62b713d9763380dbe
asd65asd65 <asd65asd65@yahoo.com.tw>
2019-12-03 13:34:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/app12/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.app12;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private Fragment[] fs = new Fragment[5];
+    private String[] titles = {"Page1","Page2","Page3"};
+    private ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+        fs[0] = new P0();
+        fs[1] = new P1();
+        fs[2] = new P2();
+        fs[3] = new P3();
+        fs[4] = new P4();
+        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        initActionBar();
+
+        viewPager.setCurrentItem(1);
+        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+
+                Log.i("onPageSelected", "Page: "+position);
+                if (position == 0){
+                    viewPager.setCurrentItem(1);
+                }else if (position == 4){
+                    viewPager.setCurrentItem(3);
+                }else{
+                    actionBar.setSelectedNavigationItem(position-1);
+                }
+            }
+        });
+    }
+
+    private void initActionBar(){
+        actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        MyTabListener myTabListener = new MyTabListener();
+
+        actionBar.addTab(actionBar.newTab().setText("Page1").setTabListener(myTabListener));
+        actionBar.addTab(actionBar.newTab().setText("Page2").setTabListener(myTabListener));
+        actionBar.addTab(actionBar.newTab().setText("Page3").setTabListener(myTabListener));
+    }
+
+    private class MyTabListener implements ActionBar.TabListener{
+
+        @Override
+        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+            viewPager.setCurrentItem(tab.getPosition()+1);
+        }
+
+        @Override
+        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        }
+
+        @Override
+        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        }
+    }
+
+    public void gotoP1(View view) {
+        viewPager.setCurrentItem(1);
+    }
+
+    public void gotoP2(View view) {
+        viewPager.setCurrentItem(2);
+    }
+
+    public void gotoP3(View view) {
+        viewPager.setCurrentItem(3);
+    }
+
+    private class MyPagerAdapter extends FragmentStatePagerAdapter{
+
+        public MyPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fs[position];
+        }
+
+        @Override
+        public int getCount() {
+            return fs.length;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            String title = "";
+            if (position != 0 && position != 4){
+                title = titles[position-1];
+            }
+
+            return title;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/app12/P1.java
@@ -0,0 +1,53 @@
+package com.example.app12;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ViewFlipper;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class P1 extends Fragment {
+    private View mainView;
+    private ViewFlipper viewFlipper;
+
+    public P1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        if (mainView == null){
+            mainView = inflater.inflate(R.layout.fragment_p1,container,false);
+            viewFlipper = mainView.findViewById(R.id.viewflipper);
+            View f0 = viewFlipper.getChildAt(0);
+            View f1 = viewFlipper.getChildAt(1);
+            View f2 = viewFlipper.getChildAt(2);
+            View f3 = viewFlipper.getChildAt(3);
+            MyClickListener listener = new MyClickListener();
+            f0.setOnClickListener(listener);
+            f1.setOnClickListener(listener);
+            f2.setOnClickListener(listener);
+            f3.setOnClickListener(listener);
+        }
+        return mainView;
+    }
+
+    private class MyClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            viewFlipper.showNext();
+        }
+    }
+
+}
